a3d9928b6d3ea3c2a05fd731faaeef1f63537679
josuehernandez <joosuee.jv@gmail.com>
2013-03-26 01:41:12 -0600

    auth.js
    definicion

---
+++ auth.js
@@ -0,0 +1,171 @@
+<!doctype html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+	<title>josue</title>
+<script>
+
+/*
+VARIABLE VALIDAS
+=======================================================
+
+x = 10
+mi_variable = 29
+
+_mi_variable = 19
+
+VARIABLES INVALIDAs
+
+mi variable (por el espacio entre ellas)
+
+=====================================================
+
+APLICACIONES con javascript
+
+soluciones con programacion
+
+====================================================
+PROGRAMACION
+--------------------------------------------------
+
+instrucciones que solo el navegador puede entener
+
+-asignar variables
+
+b = 10;
+
+-asignar constantes ( un valor que no cambia! osea es estatico ! )
+
+usuario = "josue";
+
+simbolos logicos
+--------------------------------------------------
+
+<= , >= , < , > , == , != (diferente )
+
+0 <= x <= 10
+
+expresiones logicas
+--------------------------------------------------
+
+|| = ò , && =  y ! (  negacion )
+
+PALABRAS RESERVADAS
+--------------------------------------------------
+
+true , false , else , if ,  console.log , for , while
+
+alert , prompt
+
+
+QUE ES UNA FUNCION
+
+definicion : tiene que realizar una tarea
+ 
+identificador_de_funcion()
+
+ejem.
+
+sentarme();
+
+
+function soyinteligente(){
+
+	console.log("soy jouse y aprendo rapido");
+
+}
+
+// llamamos a la funcion declarada arriba
+soyinteligente()
+
+
+function perro(){
+
+	nombre = "sky";
+	saludo = "Soy un perro y me llamo";
+
+	console.log(saludo , nombre )
+
+}
+
+perro();
+
+  
+function perro(nombre , edad ){
+ 
+	saludo = "Soy un perro y me llamo";
+
+	console.log(saludo , nombre  ,edad)
+
+}
+
+//------------------------------------
+
+
+// ( argumentos )
+perro("skypeee" , 20 ); // ( varoles1 , val2 , val3 )
+
+
+function estudiante(nombre , edad , hobbie){
+
+	console.log("nombre = "+nombre, " Mi edad "+ edad , "Mi hobbie "+hobbie)
+
+}
+
+estudiante("josue" , 18 , "hobbie" );
+
+
+´ 
+--------------------------------------------------
+VAROLES LOGICOS
+--------------------------------------------------
+mi_variable_logica = true 
+ 
+
+
+ALGORITMO ? son pasos ordenados para resolver un problema
+
+=======================================================
+acceso de tarjeta de banco
+
+1.- Ingresa contraseña
+2.- verificar codigos 
+3.- si es correcto; entonces damos los datos
+	si no te vota
+ 
+*/ 
+
+password_correcto = "hola";
+
+password = prompt("Ingresa password de tarjeta" )
+
+if(   password == password_correcto ){
+
+console.log( "contraseña correcta|")
+
+}else{
+
+
+console.log( "contraseña incorrecta")
+
+
+} // si es correcto entonces has { } 
+ 
+
+
+
+
+
+
+</script>
+
+ 
+ 
+</head>
+<body>
+
+j
+hola soy josue	
+ 
+</body>
+</html>
